9351f54d6703740046146ba7f0d3df54bd028baf
Mary Ellen Schuster <skg3cn@virginia.edu>
2024-09-30 12:24:55 -0400

    finished polynomial function

--- HW03a-1.ml
+++ HW03a-1.ml
@@ -44,7 +44,7 @@ problem below, but you'll need to create yourown test cases.
 
 
 
-(*FINISHED
+(* 1. FINISHED
  * Return the list of all elements of a given list that are less than e.
  *  
  * val less : 'a * 'a list -> 'a list 
@@ -59,7 +59,7 @@ let rec less = function
 assert (less (5, [1; 6; 3; 8; 2]) = [1; 3; 2]);;
 assert (less (3, [5; 1; 2; 4]) = [1; 2]);;
 
-(* FINISHED
+(* 2. FINISHED
  * Write a tail-recursive version of `less`. Feel free to create a helper function.
  *  
  * val less_tail : 'a * 'a list -> 'a list 
@@ -77,18 +77,22 @@ let less_tail (a,lst) =
 assert (less_tail (5, [1; 6; 3; 8; 2]) = [1; 3; 2]);;
 assert (less_tail (3, [5; 1; 2; 4]) = [1; 2]);;
 
-(*
+(* 3. FINISHED
  * Returns true if and only if list `list` contains adjacent equal elements.
  *  
  * val repeats : 'a list -> bool 
  *)
 
 let rec repeats = function
+  | [] -> false
+  | [_] -> false
+  | x::y::xs -> when x = y -> true 
+  | _ ::y::xs-> repeats (y::xs)
 
 assert (repeats [1; 2; 2; 3] = true);;
 assert (repeats [1; 2; 3; 4] = false);;
 
-(*
+(* 4. FINISHED
  * Returns the value of polynomial `polynomial` at x.
  * 
  * We represent a polynomial as a list of float coefficients,
@@ -104,11 +108,20 @@ assert (repeats [1; 2; 3; 4] = false);;
  *)
 
 let rec eval = function
+  let eval (x: float, coefficients: float list) : float = 
+    let rec aux coeffs power acc = 
+      match coeffs with 
+        | [] -> acc
+        | h::t -> aux t (power + 1) (acc +. (h*. (x ** float_of_int power)))
+        (*increment power by one, add the current value to acc, last expression evaluates to head times x to the power *)
+    in 
+    aux coefficients 0 0.0
+
 
 assert (eval (2.0, [1.0; 5.0; 3.0]) = 23.0);;
 assert (eval (1.0, [1.0; 5.0; 3.0]) = 9.0);;
 
-(*
+(* 5. 
  * Returns true if e is a member of list list.
  *
  * val is_member : 'a * 'a list -> bool
@@ -121,7 +134,7 @@ assert (is_member (5, [1; 2; 3; 4]) = false);;
 
 (* --- Several of the remaining functions build on each other --- *)
 
-(*
+(* 6. 
  * Removes all the elements after the first occurrence of `e`
  * if it is in the list `list`, otherwise returns the empty list. 
  * You do not need to preserve the order of the items in the list.
@@ -135,7 +148,7 @@ let rec remove_after_e = function
 assert (remove_after_e (3, [1; 2; 3; 4; 5]) = [3; 2; 1]);;
 assert (remove_after_e (1, [1; 2; 3]) = [1]);;
 
-(*
+(* 7. 
  * Removes the first occurrence of `e` from the list `list`
  * if `a` is in the list, otherwise returns the unmodified list.
  *  
@@ -147,7 +160,7 @@ let rec remove_first = function
 assert (remove_first (2, [1; 2; 3; 2]) = [1; 3; 2]);;
 assert (remove_first (4, [1; 2; 3]) = [1; 2; 3]);;
 
-(*
+(* 8. 
  * Removes all the occurrences of e from the list list
  * if e is in the list, otherwise returns the unmodified list.
  *  
@@ -159,7 +172,7 @@ let rec remove_all= function
 assert (remove_all (2, [1; 2; 3; 2]) = [1; 3]);;
 assert (remove_all (1, [1; 1; 1]) = []);;
 
-(*
+(* 9. 
  * Removes all the elements of list `list` where `(test_fn element)`
  * returns false, otherwise returns the unmodified list.
  *
